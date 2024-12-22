585258124251ecf567a229cc6da40e9457ed336a
Digvijayy-Git <Digs.Y@outlook.com>
2024-12-22 11:46:53 +0530

    Assignment 1 - Java Datat Types

---
+++ src/com/creatio/crm/language/basics/HelloWord.java
@@ -0,0 +1,12 @@
+package com.creatio.crm.language.basics;
+
+public class HelloWord {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello world");
+
+	}
+
+}
---
+++ src/com/creatio/crm/language/basics/JavaDataTypes.java
@@ -0,0 +1,91 @@
+package com.creatio.crm.language.basics;
+
+public class JavaDataTypes {
+
+	public static void main (String [] args)
+	{
+
+		//1Temperature of a city in degrees Celsius: 25.5
+		float Temp = 25.5f;
+		System.out.println(Temp);
+		
+		//2. Whether a customer has placed an order: true or false
+		boolean order = true;
+		System.out.println(order);
+		
+		// 3 Person's phone number: "123-456-7890"
+		int PN = 123-456-7890;
+		System.out.println(PN);
+		
+		//4. Amount of money in a customer's bank account: 1000.50
+		float Amount = 1000.50f;
+		System.out.println(Amount);
+		
+		//5. Person's email address: "john.doe@example.com"
+		String email = "john.doe@example.com";
+		System.out.println(email);
+		
+		//6. Coordinates of a location (latitude, longitude): 37.7749, -122.4194
+		float latitude = 37.7749f;
+		float longitude = -122.4194f;
+		System.out.println(latitude +" "+ longitude);
+		
+		//7. Person's Martial status: true or false
+		boolean marital_status = true;
+		System.out.println(marital_status);
+		
+		//8.Person's occupation: "Software Engineer"
+		String occupation = "Software Engineer";
+		System.out.println(occupation);
+		
+		//9. Person's Favorite color: "Blue"
+		String fav_colour = "Blue";
+		System.out.println(fav_colour);
+		
+		//10.Current year: 2023
+		short year = 2023;
+		System.out.println(year);
+		
+		//11.Number of followers on a social media platform: 1,000,000
+		int followers = 1000000;
+		System.out.println(followers);
+		
+		//12.Rating of a movie: 7.5
+		float rating = 7.5f;
+		System.out.println(rating);
+		
+		//13.Person's blood type: 'A'
+		char blood_group = 'O';
+		System.out.println(blood_group);
+		
+		//14.Title of a book: "To Kill a Mockingbird"
+		String Title = "To Kill a Mockingbird";
+		System.out.println(Title);
+		
+		//15.Number of employees in a company: 500
+		short employee = 500;
+		System.out.println(employee);
+		
+		//16.Time of an event: 2:30 PM
+		String time = "2.30 PM";
+		System.out.println(time);
+		
+		//17.Name of a country: "United States"
+		String Contry_Name = "United States";
+		System.out.println(Contry_Name);
+		
+		//18.Person's eye color: "Brown"
+		String Eye_colour = "Brown";
+		System.out.println(Eye_colour);
+		
+		//19.Person's birthplace: "New York City"
+		String Birthplace = "New York City";
+		System.out.println(Birthplace);
+		
+		//20. Distance between two cities: 200.5
+		float Distance = 200.5f;
+		System.out.println(Distance);	
+		
+	}
+
+}
